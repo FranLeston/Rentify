16b6898ce9c9e271126d333177596c07cff6cad8
Fran Leston <fran@Frans-MacBook-Air.local>
2019-04-13 11:36:52 +0200

    heroku

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,33 +1,18 @@
-// const createError = require('http-errors');
-// const cors = require('cors');
-
-// const allowedOrigins = (process.env.CORS_ORIGINS || "http://localhost:3000")
-//   .split(',')
-//   .map(o => o.trim())
-
-// module.exports = cors({
-//   origin: (origin, next) => {
-//     const allowed = !origin || allowedOrigins.indexOf(origin) !== -1;
-//     if (allowed) {
-//       next(null, allowed);
-//     } else {
-//       next(createError(401, 'Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// });
-const cors = require('cors')
 const createError = require('http-errors');
+const cors = require('cors');
 
-const allowedOrigins = ['mongodb://<lestonramos>:<NewYorkCity911$>@ds135786.mlab.com:35786/rentify-ironhack']
+const allowedOrigins = (process.env.CORS_ORIGINS || "http://localhost:3000")
+  .split(',')
+  .map(o => o.trim())
+
 module.exports = cors({
   origin: (origin, next) => {
-    const isAllowed = !origin || allowedOrigins.some(o => o === origin);
-    if(isAllowed) {
-      next(null,isAllowed);
+    const allowed = !origin || allowedOrigins.indexOf(origin) !== -1;
+    if (allowed) {
+      next(null, allowed);
     } else {
-      next(createError(401, "Not allowed by CORS"))
+      next(createError(401, 'Not allowed by CORS'));
     }
   },
   credentials: true
-})
+});
